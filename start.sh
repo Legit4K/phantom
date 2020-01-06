d57df4f413d36bf0f0b9062896dea778784599bd
Legit <44619264+Legit4K@users.noreply.github.com>
2020-01-06 12:58:17 +0000

    Multi platform support

--- start.sh
+++ start.sh
@@ -5,7 +5,6 @@ chmod u+x ./phantom-*
 if  [[ $1 = "mac" ]]; then
     echo "Running through Mac"
 			./phantom-mac -server 4b4t.online
-else
+else if  [[ $1 = "linux" ]]; then
     echo "Running through Linux"
-			./phantom-linux -server 4b4t.online
 fi 
