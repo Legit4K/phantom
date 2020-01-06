372844cc2d3067efeed692e67690796a9749283a
Legit <44619264+Legit4K@users.noreply.github.com>
2020-01-06 13:27:03 +0000

    Rewrite start script to support option parameters

--- start.sh
+++ start.sh
@@ -1,11 +1,41 @@
-#!/bin/sh
+#!/usr/bin/sh
 
-chmod u+x ./phantom-*
-
-if  [[ $1 = "mac" ]]; then
-    echo "Running through Mac"
-			./phantom-mac -server 4b4t.online
-else
-    echo "Running through Linux"
-			./phantom-linux -server 4b4t.online
+if [ $# -eq 0 ]
+then
+        echo "Missing options!"
+        echo "(run $0 -h for help)"
+        echo ""
+        exit 0
 fi
+
+ECHO="false"
+
+while getopts "hlm" OPTION; do
+        case $OPTION in
+
+								m) 
+                        echo "Running through Mac"
+												chmod u+x ./phantom-*
+												./phantom-mac -server 4b4t.online
+                        ;;
+
+                l)
+                        echo "Running through Linux"
+												chmod u+x ./phantom-*
+												./phantom-linux -server 4b4t.online
+                        ;;
+
+                h)
+                        echo "Usage:"
+                        echo "start.sh -h "
+                        echo "start.sh -l "
+												echo "start.sh -m "
+                        echo ""
+												echo "   -m         to initiate proxy on mac"
+                        echo "   -l            to initiate proxy on linux"
+                        echo "   -h           help menu (this output)"
+                        exit 0
+                        ;;
+
+        esac
+done
